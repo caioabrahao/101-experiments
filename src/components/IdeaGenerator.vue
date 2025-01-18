8e3e97144b73c7e94bd4c81c8eef25d2016ade61
Caio Abrahão <contato.caioabrahao@gmail.com>
2025-01-18 14:33:45 -0300

    Fixed Global CSS import for Production

--- src/components/IdeaGenerator.vue
+++ src/components/IdeaGenerator.vue
@@ -15,11 +15,12 @@ const interestArea = ref('');
 const skillLevel = ref('');
 const projectScale = ref('');
 
+
 const ideaPrompt = computed(() => `Generate a brief project idea pitch. 
 I want to work with ${interestArea.value}.
 Im at a ${skillLevel.value} level.
 Im looking for a ${projectScale.value} project.
-Format your response in markdown. Use emojis in the headings for extra fun!`);
+Format your response in markdown. Use emojis for extra fun!`);
 
 function handleClick(promptInput) {
     response.value = "Generating Content..."
@@ -41,59 +42,81 @@ onMounted(() => {
         <h1>Idea Generator</h1>
 
         <div class="optionsWrapper">
-            <div>
+            <div class="option-set">
                 <label>Interest Area:</label>
                 <div>
                     <input type="radio" id="interest1" value="Web Development" v-model="interestArea">
-                    <label for="interest1">Web Development</label>
+                    <label class="radio-label" for="interest1">Web Development</label>
                 </div>
                 <div>
                     <input type="radio" id="interest2" value="Graphic Design" v-model="interestArea">
-                    <label for="interest2">Graphic Design</label>
+                    <label class="radio-label" for="interest2">Graphic Design</label>
                 </div>
                 <div>
                     <input type="radio" id="interest3" value="Mobile Apps" v-model="interestArea">
-                    <label for="interest3">Mobile Apps</label>
+                    <label class="radio-label" for="interest3">Mobile Apps</label>
+                </div>
+                <div>
+                    <input type="radio" id="interest4" value="UI / UX" v-model="interestArea">
+                    <label class="radio-label" for="interest4">UI / UX</label>
+                </div>
+                <div>
+                    <input type="radio" id="interest5" value="Digital Art" v-model="interestArea">
+                    <label class="radio-label" for="interest5">Digital Art</label>
+                </div>
+                <div>
+                    <input type="radio" id="interest6" value="Electronics / Robotics" v-model="interestArea">
+                    <label class="radio-label" for="interest6">Electronics / Robotics</label>
                 </div>
             </div>
 
-            <div>
+            <div class="option-set">
                 <label>Skill Level:</label>
                 <div>
-                    <input type="radio" id="skill1" value="Beginner" v-model="skillLevel">
-                    <label for="skill1">Beginner</label>
+                    <input type="radio" id="skill1" value="First Timer" v-model="skillLevel">
+                    <label class="radio-label" for="skill1">First timer</label>
                 </div>
                 <div>
-                    <input type="radio" id="skill2" value="Intermediate" v-model="skillLevel">
-                    <label for="skill2">Intermediate</label>
+                    <input type="radio" id="skill2" value="Beginner (don't do habit tracker or todo apps)" v-model="skillLevel">
+                    <label class="radio-label" for="skill2">Beginner</label>
                 </div>
                 <div>
-                    <input type="radio" id="skill3" value="Advanced" v-model="skillLevel">
-                    <label for="skill3">Advanced</label>
+                    <input type="radio" id="skill3" value="Intermediate" v-model="skillLevel">
+                    <label class="radio-label" for="skill3">Intermediate</label>
+                </div>
+                <div>
+                    <input type="radio" id="skill4" value="Advanced" v-model="skillLevel">
+                    <label class="radio-label" for="skill4">Advanced</label>
+                </div>
+                <div>
+                    <input type="radio" id="skill5" value="Stretch my Abilities " v-model="skillLevel">
+                    <label class="radio-label" for="skill5">Stretch my Abilities </label>
                 </div>
             </div>
 
-            <div>
+            <div class="option-set">
                 <label>Project Scale:</label>
                 <div>
                     <input type="radio" id="scale1" value="Afternoon Passtime" v-model="projectScale">
-                    <label for="scale1">Afternoon Passtime</label>
+                    <label class="radio-label" for="scale1">Afternoon Passtime</label>
                 </div>
                 <div>
                     <input type="radio" id="scale2" value="Vacation Project" v-model="projectScale">
-                    <label for="scale2">Vacation Project</label>
+                    <label class="radio-label" for="scale2">Vacation Project</label>
                 </div>
                 <div>
                     <input type="radio" id="scale3" value="Startup Potential" v-model="projectScale">
-                    <label for="scale3">Startup Potential</label>
+                    <label class="radio-label" for="scale3">Startup Potential</label>
                 </div>
             </div>
+
         </div>
 
 
         <button class="btn-primary" @click="handleClick(ideaPrompt)">Generate Idea</button>
 
         <div v-if="debugMode === true">
+            <h4>Debugging Display</h4>
             <p>{{ ideaPrompt }}</p>
             <p style="opacity: .5">SELECTED OPTIONS: {{ interestArea }} | {{ skillLevel }} | {{ projectScale }}</p>
 
@@ -104,6 +127,41 @@ onMounted(() => {
 
 
 <style scoped>
+/* Radio Button Styles */
+.optionsWrapper{
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    gap: 32px;
+}
+
+.option-set{
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+}
+input[type="radio"] {
+    display: none;
+}
+.radio-label {
+    display: inline-block;
+    padding: 10px 20px;
+    border: 2px solid #ccc;
+    border-radius: 16px;
+    cursor: pointer;
+    transition: background-color 0.1s, border-color 0.1s;
+
+    width: 100%;
+}
+input[type="radio"]:checked + .radio-label {
+    background-color: #007BFF;
+    border-color: #007BFF;
+    color: white;
+}
+.radio-label:hover {
+    background-color: #a7a7a7;
+}
+
 .section{
     display: flex;
     flex-direction: column;
@@ -114,22 +172,12 @@ onMounted(() => {
     min-height: 90vh;
 }
 
-.optionsWrapper{
-    display: flex;
-    justify-content: space-around;
-    gap: 32px;
-}
-
 .responseText{
     width: 60%;	
     padding: 32px;
 
     border: 1px solid #ccc;
     border-radius: 30px;
-
-    h1{
-        font-size: 2rem;
-    }
 }
 
 </style>
---
+++ src/pages/learn/frameworks-to-chose.md
@@ -0,0 +1,14 @@
+---
+layout: ../../layouts/MarkdownLayout.astro
+title: "Qual framework escolher?"
+---
+# Qual framework escolher?
+
+Disponibilizamos vários frameworks para o site de template, para atender a maioria dos gostos de cada um. Se não sabe qual escolher, recomendo usar o JavaScript Vanilla para começar.
+
+## Qual escolher?
+- **Vanilla JS**: Para quem não sabe exatamente qual escolher e quer a experiência padrão de desenvolvimento web.
+- **Astro**: Para quem está interessado em aprender a usar um framework versátil e de fácil uso. As páginas podem ser feitas usando Markdown e o template automaticamente formatará a página para HTML
+- **Vue**: Para quem já está acostumado ou está disposto a aprender a usar o famoso framework. Boa documentação, amigável para iniciantes e excelente para criar um site responsivo e dinâmico.
+- **React**: Para quem já está acostumado ou disposto a aprender a usar o framework mais popular atualmente. Um pouco menos amigável para iniciantes, mas muito valioso para ter no portfólio
+- **HUGO**: Para quem não quer criar páginas simples, somente usando Markdown. Uma opção simples para criar páginas estáticas com o famoso gerador de sites estáticos.
